a346790061dc09534172246894668a1dc688b6c7
surjanthakur <tsurjan506@gmail.com>
2026-03-19 21:53:36 +0530

    save

--- tweety/main.py
+++
@@ -1,9 +0,0 @@
-from fastapi import FastAPI
-from database.database import lifespan
-from router import users, tweets
-
-
-app = FastAPI(lifespan=lifespan)
-
-app.include_router(router=users.router)
-app.include_router(router=tweets.tweet_router)
