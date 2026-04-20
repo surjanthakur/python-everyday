0769640e41be72df837a16a552c16b3e6c88546a
surjanthakur <tsurjan506@gmail.com>
2026-04-21 00:32:15 +0530

    s

---
+++ 005.py
@@ -0,0 +1,1 @@
+# Practice Problem: Write a single-line list comprehension that takes a list of strings, filters out strings shorter than 4 characters, and converts the remaining strings to uppercase.
