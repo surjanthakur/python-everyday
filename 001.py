2d15f554f8c4dc9e3a4527cd1e2b6f5918bfde7f
surjanthakur <tsurjan506@gmail.com>
2026-04-20 10:18:50 +0530

    s

--- 001.py
+++ 001.py
@@ -14,3 +14,14 @@ while number < 5:
 
     number += 1
     print("")
+
+
+# using for loop print same pattern
+
+i = 1
+
+while i < 5:
+    for j in range(1, i + 1):
+        print("*", end=" ")
+    i += 1
+    print()
