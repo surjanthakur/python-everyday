3f01e1d3654cb040294e71fd1a9412325b7b4357
surjanthakur <tsurjan506@gmail.com>
2026-04-21 00:40:27 +0530

    save

--- 005.py
+++ 005.py
@@ -1,1 +1,7 @@
 # Practice Problem: Write a single-line list comprehension that takes a list of strings, filters out strings shorter than 4 characters, and converts the remaining strings to uppercase.
+
+words = ["apple", "bat", "cherry", "dog", "elderberry"]
+
+filter_words = [x.upper() for x in words if len(x) >= 4]
+
+print(filter_words)
