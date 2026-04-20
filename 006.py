d0e1d4f30bf11a5483f28ed66c7dd90bb1307625
surjanthakur <tsurjan506@gmail.com>
2026-04-21 00:42:39 +0530

    s

---
+++ 006.py
@@ -0,0 +1,7 @@
+#  Dictionary Merging with Logic =>
+
+# Practice Problem: Write a function that merges two dictionaries. If a key exists in both dictionaries, sum their values. If a key exists in only one, include it as is.
+
+
+dict_a = {"a": 10, "b": 20}
+dict_b = {"b": 5, "c": 15}
