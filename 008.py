c9b01ccd61bcd96745cd48220056d96b5b83922c
surjanthakur <tsurjan506@gmail.com>
2026-04-21 01:40:49 +0530

    s

--- 008.py
+++ 008.py
@@ -1,1 +1,15 @@
 # Practice Problem: Write a function that determines if two strings are anagrams (contain the exact same characters in a different order).
+
+
+word1 = "listen"
+word2 = "silent"
+
+
+def is_anagrams(str1: str, str2: str):
+    s1 = sorted(str1.lower().replace(" ", ""))
+    s2 = sorted(str2.lower().replace(" ", ""))
+
+    return s1 == s2
+
+
+print(is_anagrams(str1=word1, str2=word2))
