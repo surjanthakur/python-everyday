2d90554a6a8f84f9dff1677b1b0387b302010896
surjanthakur <tsurjan506@gmail.com>
2026-04-21 01:01:21 +0530

    s

--- 006.py
+++ 006.py
@@ -5,3 +5,14 @@
 
 dict_a = {"a": 10, "b": 20}
 dict_b = {"b": 5, "c": 15}
+
+
+def merg_dict(dict1: dict, dict2: dict):
+    result = dict1.copy()
+    for key, val in dict2.items():
+        result[key] = result.get(key, 0) + val
+
+    print(result)
+
+
+merg_dict(dict1=dict_a, dict2=dict_b)
