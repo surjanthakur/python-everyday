52d06bc94acbd97882f95e86a3eebd6583ec5600
surjanthakur <tsurjan506@gmail.com>
2026-04-26 02:00:59 +0530

    s

--- websockets-server/main.py
+++ websockets-server/main.py
@@ -18,8 +18,21 @@ html = """
         </form>
         <ul id='messages'>
         </ul>
-        <script>
-        # add websocket logic here
+      <script>
+            let ws = new WebSocket("ws://localhost:8000/ws/connection");
+            ws.onmessage = function(event) {
+                let messages = document.getElementById('messages')
+                let message = document.createElement('li')
+                let content = document.createTextNode(event.data)
+                message.appendChild(content)
+                messages.appendChild(message)
+            };
+            function sendMessage(event) {
+                let input = document.getElementById("messageText")
+                ws.send(input.value)
+                input.value = ''
+                event.preventDefault()
+            }
         </script>
     </body>
 </html>
