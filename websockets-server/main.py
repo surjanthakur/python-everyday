b131f7e1db05b76b45eb91ef8374eebb0ffebbf2
surjanthakur <tsurjan506@gmail.com>
2026-04-26 01:48:36 +0530

    s

--- websockets-server/main.py
+++ websockets-server/main.py
@@ -1,2 +1,36 @@
-from fastapi import websockets
+from fastapi import FastAPI, WebSocket, WebSocketDisconnect, WebSocketException
 from fastapi.responses import HTMLResponse
+
+app = FastAPI()
+
+
+html = """
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Chat</title>
+    </head>
+    <body>
+        <h1>WebSocket Chat</h1>
+        <form action="" onsubmit="sendMessage(event)">
+            <input type="text" id="messageText" autocomplete="off"/>
+            <button>Send</button>
+        </form>
+        <ul id='messages'>
+        </ul>
+        <script>
+        # add websocket logic here
+        </script>
+    </body>
+</html>
+"""
+
+
+@app.get("/")
+async def html_template():
+    return HTMLResponse(content=html, status_code=200)
+
+
+@app.websocket("/ws/connection")
+async def websocket_connection(websocket: WebSocket):
+    pass
