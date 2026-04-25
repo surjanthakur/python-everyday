e9f8b1996aa92f8c1ec8841eb50318956047b4de
surjanthakur <tsurjan506@gmail.com>
2026-04-25 22:13:24 +0530

    s

--- problem-solving/18.py
+++ problem-solving/18.py
@@ -5,20 +5,5 @@ d1 = {"a": 1, "b": 2}
 d2 = {"b": 3, "c": 4}
 
 
-def merge_dict(dict1: dict, dict2: dict) -> dict:
-    new_dict = {}
-    all_keys = set(dict1.keys()) | set(dict2.keys())
-    for key in all_keys:
-        values = []
-        if key in dict1:
-            values.append(dict1[key])
-        if key in dict2:
-            values.append(dict2[key])
-        new_dict[key] = values
-
-    return new_dict
-
-
-ans = merge_dict(d1, d2)
-
-print(ans)
+def merge_dict(d1: dict, d2: dict):
+    all_keys = set(d1.keys) | set(d2.keys)
