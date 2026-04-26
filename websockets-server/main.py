c69eddeb6b8e100d4ef08fac4182087037d85dfb
surjanthakur <tsurjan506@gmail.com>
2026-04-27 02:09:45 +0530

    save

--- websockets-server/main.py
+++ websockets-server/main.py
@@ -1,55 +1,11 @@
-from fastapi import FastAPI, WebSocket, WebSocketDisconnect, WebSocketException
-from fastapi.responses import HTMLResponse
+from pathlib import Path
+
+from fastapi import FastAPI, WebSocket, WebSocketDisconnect
+from fastapi.responses import FileResponse
 import uvicorn
 
 app = FastAPI()
-
-
-html = """
-<!DOCTYPE html>
-<html>
-    <head>
-        <title>Chat</title>
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
-    </head>
-    <body>
-        <h1>WebSocket Chat</h1>
-
-        <h2 class="text-danger">your id: <span class="text-danger" id="ws-id"></span></h2>
-   
-        <form action="" class="mt-5" onsubmit="sendMessage(event)">
-            <input type="text" class="form control" id="messageText" autocomplete="off"/>
-            <button>Send</button>
-        </form>
-
-        <ul id='messages' class="mt-5>
-        </ul>
-
-      <script>
-              let client_id = Date.now();
-              window.addEventListener('DOMContentLoaded', () => {
-                document.getElementById('ws-id').textContent = client_id;
-              });
-         
-               let ws = new WebSocket("ws://localhost:8000/ws/connection/${client_id}");
-            ws.onmessage = function(event) {
-                let messages = document.getElementById('messages');
-                let message = document.createElement('li')
-                let content = document.createTextNode(event.data)
-                message.appendChild(content)
-                messages.appendChild(message)
-            };
-            function sendMessage(event) {
-                let input = document.getElementById("messageText")
-                ws.send(input.value)
-                input.value = ''
-                event.preventDefault()
-            }
-        </script>
-
-    </body>
-</html>
-"""
+BASE_DIR = Path(__file__).parent
 
 
 class ConnectionManager:
@@ -79,7 +35,7 @@ socket_manager = ConnectionManager()
 
 @app.get("/")
 async def html_template():
-    return HTMLResponse(content=html, status_code=200)
+    return FileResponse(BASE_DIR / "index.html")
 
 
 @app.websocket("/ws/connection/{client_id}")
@@ -87,7 +43,7 @@ async def websocket_endpoint(websocket: WebSocket, client_id: int):
     try:
         await socket_manager.connect_connection(websocket)
         while True:
-            data = websocket.receive_text()
+            data = await websocket.receive_text()
             await socket_manager.send_messages(f"you wrote: {data}", websocket)
             await socket_manager.brodcast_messages(f"client {client_id} says: {data}")
     except WebSocketDisconnect:
