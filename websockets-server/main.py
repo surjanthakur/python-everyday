ed5b2947d8ad82c0ae49d8a6d0e583c07c7429d2
surjanthakur <tsurjan506@gmail.com>
2026-04-27 02:18:56 +0530

    s

--- websockets-server/main.py
+++ websockets-server/main.py
@@ -1,11 +1,55 @@
-from pathlib import Path
-
 from fastapi import FastAPI, WebSocket, WebSocketDisconnect
-from fastapi.responses import FileResponse
+from fastapi.responses import HTMLResponse
 import uvicorn
 
 app = FastAPI()
-BASE_DIR = Path(__file__).parent
+
+
+html = """
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Chat</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+    </head>
+    <body>
+        <h1>WebSocket Chat</h1>
+
+        <h2 class="text-danger">your id: <span class="text-danger" id="ws-id"></span></h2>
+   
+        <form action="" class="mt-5" onsubmit="sendMessage(event)">
+            <input type="text" class="form control" id="messageText" autocomplete="off"/>
+            <button>Send</button>
+        </form>
+
+        <ul id='messages' class="mt-5>
+        </ul>
+
+      <script>
+              let client_id = int(Date.now());
+              window.addEventListener('DOMContentLoaded', () => {
+                document.getElementById('ws-id').textContent = str(client_id);
+              });
+         
+               let ws = new WebSocket("ws://localhost:8000/ws/connection/${client_id}");
+            ws.onmessage = function(event) {
+                let messages = document.getElementById('messages');
+                let message = document.createElement('li')
+                let content = document.createTextNode(event.data)
+                message.appendChild(content)
+                messages.appendChild(message)
+            };
+            function sendMessage(event) {
+                let input = document.getElementById("messageText")
+                ws.send(input.value)
+                input.value = ''
+                event.preventDefault()
+            }
+        </script>
+
+    </body>
+</html>
+"""
 
 
 class ConnectionManager:
@@ -35,7 +79,7 @@ socket_manager = ConnectionManager()
 
 @app.get("/")
 async def html_template():
-    return FileResponse(BASE_DIR / "index.html")
+    return HTMLResponse(content=html)
 
 
 @app.websocket("/ws/connection/{client_id}")
