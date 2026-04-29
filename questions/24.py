bad9e0d3f2ab46497330f37760dfa1b6c03388a5
surjanthakur <tsurjan506@gmail.com>
2026-04-29 20:48:55 +0530

    s

---
+++ questions/24.py
@@ -0,0 +1,10 @@
+# Two Sum (Optimized)
+
+# Problem: Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+nums = [2, 7, 11, 15]
+target = 9
+
+
+def sum_n(num: list, target: int):
+    pass
