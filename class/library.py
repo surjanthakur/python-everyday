27ebea059772fcde4c0f112f54470967a4704285
surjanthakur <tsurjan506@gmail.com>
2026-06-17 22:48:07 +0530

    s

--- class/library.py
+++ class/library.py
@@ -1,16 +1,30 @@
-class Library:
-    library_name = "NYC state 📚"
-    def __init__(self):
-        self.__all_books =[]
-    
-    def get_books(self):
-        print(f"all books => {self.__all_books.items()}")
-
-    def add_book(self , book:dict):
-        self.__all_books.update(book)
-        print("book added")
-
 class Book:
     def __init__(self , author:str , title:str):
         self.author  = author
         self.title = title
+    
+    def __str__(self):
+        return f"author:{self.author} , book: {self.title}"
+
+class Library:
+    library_name = "NYC state 📚"
+
+    def __init__(self):
+        self.__all_books:list[Book] = []
+    
+    def get_books(self):
+        return self.__all_books
+
+    def add_book(self , book:Book):
+        self.__all_books.append(book)
+
+library = Library()
+
+b1 = Book("James Clear", "Atomic Habits")
+b2 = Book("Robert Kiyosaki", "Rich Dad Poor Dad")
+
+library.add_book(b1)
+library.add_book(b2)
+
+for book in library.get_books():
+    print(book)
